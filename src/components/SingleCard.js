71a3f395cf1e1a8cfd00bdd2a7d0b034911c5cb0
ENGMohamedFarhan <mohamed12mg6@gmail.com>
2026-01-17 17:06:23 +0300

    Initial Magic-Memory project setup

---
+++ src/components/SingleCard.js
@@ -0,0 +1,22 @@
+import './SingleCard.css'
+import React from 'react'
+
+export default function SingleCard({card,handleChoice,flipped,disabled}) {
+  const handleClick=()=>{
+    if (!disabled){
+        handleChoice(card)
+    }
+}
+  return (
+<div className='card'>
+      <div className={flipped ? "flipped" : ""}>
+        <img className='front' src={card.src} alt='Card front' />
+        <img 
+        className='back'
+         src="/img/cover.png"
+         onClick={handleClick} 
+         alt='back front' /> 
+      </div>
+     </div>
+  )
+}
